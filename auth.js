1ca93eaf5d9e6908b6400ac782fee51c5b3f0c5d
Mahammedi Abdelghani <wboductqr@relay.firefox.com>
2022-10-12 12:30:32 +0100

    update: GITHUB_CLIENT_CALLBACK_URL

--- auth.js
+++ auth.js
@@ -52,7 +52,7 @@ module.exports = function(app, myDataBase) {
   passport.use(new GitHubStrategy({
     clientID: process.env.GITHUB_CLIENT_ID,
     clientSecret: process.env.GITHUB_CLIENT_SECRET,
-    callbackURL: 'https://advancednode.abdelghanymh.repl.co/auth/github/callback'
+    callbackURL: `${process.env.GITHUB_CLIENT_CALLBACK_URL}/auth/github/callback`
   },
     function(accessToken, refreshToken, profile, cb) {
       // console.log(profile);
